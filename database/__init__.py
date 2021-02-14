14c4ddc6aa5646ae62653f719b623eb8725dd528
jloesch30 <61180397+jloesch30@users.noreply.github.com>
2021-02-14 14:53:24 -0600

    boto req

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,6 @@
 from mongoengine import connect
 from dotenv import load_dotenv
-from boto.s3.connection import S3Connection
+# from boto.s3.connection import S3Connection
 import os
 
 # ----------
@@ -8,7 +8,7 @@ import os
 # ----------
 print('Connecting to DB')
 load_dotenv()
-s3 = S3Connection(os.environ['DB_CONNECT_URL'])
+# s3 = S3Connection(os.environ['DB_CONNECT_URL'])
 db_connect = os.getenv('DB_CONNECT_URL')
 connect('mydb', host=str(db_connect))
 print('Connection successful')
